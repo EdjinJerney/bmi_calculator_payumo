4a341fe9c14e36e365674c186fce92e8c455ad2b
Edjin Jerney <103238715+EdjinJerney@users.noreply.github.com>
2023-11-26 14:25:42 +0800

    added gesture detector for gender selection

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,7 +5,10 @@ import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFEB1555);
+
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,6 +16,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,16 +31,30 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableCard(
-                    color: activeCardColor,
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    color: (selectedGender == Gender.male)
+                        ? activeCardColor
+                        : inactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.mars,
-                      label: 'Male',
+                      label: 'MALE',
                     ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    color: activeCardColor,
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    color: (selectedGender == Gender.female)
+                        ? activeCardColor
+                        : inactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.venus,
                       label: 'FEMALE',
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.color, this.cardChild, this.onPress});
   final Color color;
   final Widget? cardChild;
-  ReusableCard({required this.color, this.cardChild});
+  final VoidCallback? onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        color: color, //Color(0xFF1D1E33)
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: color, //Color(0xFF1D1E33)
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
